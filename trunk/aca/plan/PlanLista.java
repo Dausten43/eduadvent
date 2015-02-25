ffee92b6bb1f82bcf73d88921fba0651550f8d81
etorres <etorres@4319675a-d343-5740-a059-03ad501970ae>
2015-02-25 15:33:15 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@102 4319675a-d343-5740-a059-03ad501970ae

--- trunk/aca/plan/PlanLista.java
+++ trunk/aca/plan/PlanLista.java
@@ -3,6 +3,7 @@ package aca.plan;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PlanLista {
 	public ArrayList<Plan> getListAll(Connection conn, String escuelaId, String orden ) throws SQLException{
@@ -115,6 +116,30 @@ public class PlanLista {
 		}				
 		
 		return lisPlan;
+	}
+	
+	public HashMap<String, Plan> mapPlanesEscuela(Connection conn, String escuelaId) throws SQLException{
+		HashMap<String, Plan> map 	= new HashMap<String, Plan>();
+		Statement st 						= conn.createStatement();
+		ResultSet rs 						= null;
+		String comando						= "";
+		
+		try{
+			comando = "SELECT PLAN_ID, PLAN_NOMBRE, NIVEL_ID, ESCUELA_ID, ESTADO, VALIDA_HORARIO, TITULO FROM PLAN WHERE ESCUELA_ID = '"+escuelaId+"'";			
+			rs = st.executeQuery(comando);
+			
+			while (rs.next()){
+				Plan plan = new Plan();
+				plan.mapeaReg(rs);
+				map.put(plan.getPlanId(), plan);
+			}
+		}catch(Exception ex){
+			System.out.println("Error - aca.plan.PlanLista|mapPlanesEscuela|:"+ex);
+		}finally{
+			if (rs!=null) rs.close();
+			if (st!=null) st.close();
+		}
+		return map;
 	}
 	
 }
