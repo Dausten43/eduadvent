064bc2cc81902cc57ea9ed4bef4f2cccb775ef49
etorres <etorres@4319675a-d343-5740-a059-03ad501970ae>
2015-03-19 14:51:40 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@194 4319675a-d343-5740-a059-03ad501970ae

--- trunk/aca/ciclo/CicloBloqueLista.java
+++ trunk/aca/ciclo/CicloBloqueLista.java
@@ -67,6 +67,38 @@ public class CicloBloqueLista {
 		return lisCicloBloque;
 	}
 	
+	public ArrayList<CicloBloque> getListCiclo(Connection conn, String cicloId, String promedioId, String orden ) throws SQLException{
+		ArrayList<CicloBloque> lisCicloBloque 	= new ArrayList<CicloBloque>();
+		Statement st 	= conn.createStatement();
+		ResultSet rs 	= null;
+		String comando	= "";
+		
+		try{
+			comando = " SELECT CICLO_ID, BLOQUE_ID, BLOQUE_NOMBRE,"
+					+ " TO_CHAR(F_INICIO,'DD/MM/YYYY') AS F_INICIO,"
+					+ " TO_CHAR(F_FINAL,'DD/MM/YYYY') AS F_FINAL, VALOR, ORDEN, PROMEDIO_ID, CORTO, DECIMALES"
+					+ " FROM CICLO_BLOQUE"
+					+ " WHERE CICLO_ID = '"+cicloId+"'"
+					+ " AND PROMEDIO_ID = '"+promedioId+"' "+orden;
+			
+			rs = st.executeQuery(comando);			
+			while (rs.next()){
+				
+				CicloBloque ciclo = new CicloBloque();				
+				ciclo.mapeaReg(rs);
+				lisCicloBloque.add(ciclo);
+			}
+			
+		}catch(Exception ex){
+			System.out.println("Error - aca.ciclo.cicloBloqueLista|getListCiclo|:"+ex);
+		}finally{
+			if (rs!=null) rs.close();
+			if (st!=null) st.close();
+		}	
+
+		return lisCicloBloque;
+	}
+	
 	public ArrayList<CicloBloque> getBloquePromedioCiclo(Connection conn, String promedioId, String cicloId, String orden ) throws SQLException{
 		ArrayList<CicloBloque> lisCicloBloque 	= new ArrayList<CicloBloque>();
 		Statement st 	= conn.createStatement();
