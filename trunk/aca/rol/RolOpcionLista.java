8f84631823f5dca32ca508ce7db58b424421d206
anthony <anthony@4319675a-d343-5740-a059-03ad501970ae>
2015-11-12 17:24:18 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@643 4319675a-d343-5740-a059-03ad501970ae

--- trunk/aca/rol/RolOpcionLista.java
+++ trunk/aca/rol/RolOpcionLista.java
@@ -12,7 +12,7 @@ public class RolOpcionLista {
 		String comando				= "";
 		
 		try{
-			comando = "SELECT * FROM ROL "+ orden;
+			comando = "SELECT * FROM ROL_OPCION "+ orden;
 			
 			rs = st.executeQuery(comando);
 			while (rs.next()){
@@ -22,7 +22,33 @@ public class RolOpcionLista {
 			}
 			
 		}catch(Exception ex){
-			System.out.println("Error - aca.rol.Rol|getListAll|:"+ex);
+			System.out.println("Error - aca.rol.RolOpciones|getListAll|:"+ex);
+		}finally{
+			if (rs!=null) rs.close();
+			if (st!=null) st.close();
+		}				
+		
+		return list;
+	}
+	
+	public ArrayList<Rol> getList(Connection conn, String rolId, String orden ) throws SQLException{
+		ArrayList<Rol> list 	= new ArrayList<Rol>();
+		Statement st 				= conn.createStatement();
+		ResultSet rs 				= null;
+		String comando				= "";
+		
+		try{
+			comando = "SELECT * FROM ROL_OPCION WHERE ROL_ID='"+rolId+"'"+ orden;
+			
+			rs = st.executeQuery(comando);
+			while (rs.next()){
+				Rol obj = new Rol();
+				obj.mapeaReg(rs);
+				list.add(obj);
+			}
+			
+		}catch(Exception ex){
+			System.out.println("Error - aca.rol.RolOpciones|getListAll|:"+ex);
 		}finally{
 			if (rs!=null) rs.close();
 			if (st!=null) st.close();
