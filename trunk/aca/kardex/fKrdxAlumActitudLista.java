ea2b9d092429b54613ef3693b313796665937c69
anthony <anthony@4319675a-d343-5740-a059-03ad501970ae>
2016-02-26 17:49:40 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@1188 4319675a-d343-5740-a059-03ad501970ae

---
+++ trunk/aca/kardex/fKrdxAlumActitudLista.java
@@ -0,0 +1,42 @@
+
+package aca.kardex;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+
+public class fKrdxAlumActitudLista {
+	
+	public ArrayList<KrdxAlumActitud> getListAll(Connection conn, String orden ) throws SQLException{
+		ArrayList<KrdxAlumActitud> lis 	= new ArrayList<KrdxAlumActitud>();
+		Statement st 	= conn.createStatement();
+		ResultSet rs 	= null;
+		String comando	= "";
+		
+		try{
+			comando = "SELECT CODIGO_ID, CICLO_GRUPO_ID, CURSO_ID," +
+                " EVALUACION_ID, ASPECTOS_ID, NOTA " +
+                " FROM KRDX_ALUM_ACTITUD "+orden;
+			
+			rs = st.executeQuery(comando);			
+			while (rs.next()){
+				
+				KrdxAlumActitud obj = new KrdxAlumActitud();				
+				obj.mapeaReg(rs);
+				lis.add(obj);
+			}
+			
+		}catch(Exception ex){
+			System.out.println("Error - aca.kardex.KrdxAlumConductaLista|getListAll|:"+ex);
+		}finally{
+			if (rs!=null) rs.close();
+			if (st!=null) st.close();
+		}	
+		
+		return lis;
+	}
+}
