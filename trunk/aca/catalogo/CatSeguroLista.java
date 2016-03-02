2d6dcd76881afd770c0f6bd4269e302349f5a55d
anthony <anthony@4319675a-d343-5740-a059-03ad501970ae>
2016-03-02 17:59:15 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@1223 4319675a-d343-5740-a059-03ad501970ae

--- trunk/aca/catalogo/CatSeguroLista.java
+++ trunk/aca/catalogo/CatSeguroLista.java
@@ -8,19 +8,19 @@ import java.util.ArrayList;
 
 public class CatSeguroLista {
 	
-	public ArrayList<CatActividadEtiqueta> getListAll(Connection conn, String escuelaId, String orden) throws SQLException{
-		ArrayList<CatActividadEtiqueta> list 	= new ArrayList<CatActividadEtiqueta>();
-		Statement st 							= conn.createStatement();
-		ResultSet rs 							= null;
-		String comando							= "";
+	public ArrayList<CatSeguro> getListAll(Connection conn, String escuelaId, String orden) throws SQLException{
+		ArrayList<CatSeguro> list 	= new ArrayList<CatSeguro>();
+		Statement st 				= conn.createStatement();
+		ResultSet rs 				= null;
+		String comando				= "";
 		
 		try{
 			comando = "SELECT * " +
-					"FROM CAT_SEGURO WHERE ESCUELA_ID = TO_NUMBER('"+escuelaId+"', '999') "+orden;		
+					"FROM CAT_SEGURO WHERE ESCUELA_ID = '"+escuelaId+"' "+orden;		
 			
 			rs = st.executeQuery(comando);			
 			while (rs.next()){
-				CatActividadEtiqueta obj = new CatActividadEtiqueta();				
+				CatSeguro obj = new CatSeguro();				
 				obj.mapeaReg(rs);
 				list.add(obj);
 			}
