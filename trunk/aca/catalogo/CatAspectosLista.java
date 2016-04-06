7d28518e7dd7f5d9645969a07cdfcd26571e071d
etorres <etorres@4319675a-d343-5740-a059-03ad501970ae>
2016-04-06 14:04:19 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@1445 4319675a-d343-5740-a059-03ad501970ae

--- trunk/aca/catalogo/CatAspectosLista.java
+++ trunk/aca/catalogo/CatAspectosLista.java
@@ -64,6 +64,36 @@ public class CatAspectosLista {
 		return list;
 	}
 	
+	public ArrayList<CatAspectos> getListAspectos(Connection conn, String escuelaId, String nivelId, String orden ) throws SQLException{
+		ArrayList<CatAspectos> list 	= new ArrayList<CatAspectos>();
+		Statement st 	= conn.createStatement();
+		ResultSet rs 	= null;
+		String comando	= "";
+		
+		try{
+			comando = " SELECT ESCUELA_ID, ASPECTOS_ID, NOMBRE, ORDEN, NIVEL_ID, AREA_ID"
+					+ " FROM CAT_ASPECTOS"
+					+ " WHERE ESCUELA_ID = '"+escuelaId+"'"
+					+ " AND NIVEL_ID = TO_NUMBER('"+nivelId+"','99') "+orden;
+			
+			rs = st.executeQuery(comando);			
+			while (rs.next()){
+				
+				CatAspectos obj = new CatAspectos();				
+				obj.mapeaReg(rs);
+				list.add(obj);
+			}
+			
+		}catch(Exception ex){
+			System.out.println("Error - aca.catalogo.CatAspectosLista|getListAspectos|:"+ex);
+		}finally{
+			if (rs!=null) rs.close();
+			if (st!=null) st.close();
+		}		
+		
+		return list;
+	}
+	
 	public HashMap<String,CatAspectos> getMapAll(Connection conn, String escuelaId, String orden ) throws SQLException{
 		
 		HashMap<String,CatAspectos> map = new HashMap<String,CatAspectos>();
