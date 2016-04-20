ce07f5feddf49a6861df782a7a7a1c71b405e50f
etorres <etorres@4319675a-d343-5740-a059-03ad501970ae>
2016-04-20 20:24:00 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@1670 4319675a-d343-5740-a059-03ad501970ae

--- trunk/aca/empleado/EmpCurriculumLista.java
+++ trunk/aca/empleado/EmpCurriculumLista.java
@@ -37,4 +37,35 @@ public class EmpCurriculumLista {
 		
 		return listEmp;
 	}
+	
+	public ArrayList<EmpCurriculum> lisEmpleados(Connection conn, String escuelaId, String orden) throws SQLException{
+		
+		ArrayList<EmpCurriculum> listEmp		= new ArrayList<EmpCurriculum>();
+		Statement st 			= conn.createStatement();
+		ResultSet rs 			= null;
+		String comando	= "";
+		
+		try{
+			comando = " SELECT ID_EMPLEADO, LUGAR_NAC, T_PROFESIONAL, G_POSGRADO,"
+					+ " T_UNIVERSITARIO, RESP_ACTUAL, RESP_ANTERIOR, EXP_DOCENTE, TO_CHAR(F_NACIMIENTO,'DD/MM/YYYY') AS F_NACIMIENTO,"
+					+ " COALESCE(NACIONALIDAD,91) AS NACIONALIDAD"
+					+ " FROM EMP_CURRICULUM"
+					+ " WHERE SUBSTR(ID_EMPLEADO,1,3) = '"+escuelaId+"'"+orden;			
+			rs = st.executeQuery(comando);
+			while (rs.next()){
+				
+				EmpCurriculum emp = new EmpCurriculum();
+				emp.mapeaReg(rs);
+				listEmp.add(emp);
+			}
+			
+		}catch(Exception ex){
+			System.out.println("Error - aca.empleado.EmpCurriculumLista|getListAll|:"+ex);
+		}finally{
+			if (rs!=null) rs.close();
+			if (st!=null) st.close();
+		}
+		
+		return listEmp;
+	}
 }
