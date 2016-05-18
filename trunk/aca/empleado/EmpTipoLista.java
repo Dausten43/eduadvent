e1caec5e5d429c90f80f479214ce49508ece8424
sem <sem@4319675a-d343-5740-a059-03ad501970ae>
2016-05-18 15:55:26 +0000

    git-svn-id: https://172.16.7.97:8443/svn/escuela@1787 4319675a-d343-5740-a059-03ad501970ae

--- trunk/aca/empleado/EmpTipoLista.java
+++ trunk/aca/empleado/EmpTipoLista.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EmpTipoLista {
 	
@@ -64,6 +65,32 @@ public class EmpTipoLista {
 		}		
 		
 		return lisTipo;
+	}
+	
+	public static HashMap<String, String> mapTipoEmpleados(Connection conn, String orden) throws SQLException{
+		
+		HashMap<String,String> map 	= new HashMap<String,String>();
+		Statement st 		= conn.createStatement();
+		ResultSet rs 		= null;
+		String comando		= "";		
+		
+		try{
+			comando = " SELECT TIPO_ID, TIPO_NOMBRE FROM EMP_TIPO "+orden;
+					
+			rs = st.executeQuery(comando);
+			
+			while (rs.next()){				
+				map.put(rs.getString("TIPO_ID"), rs.getString("TIPO_NOMBRE"));
+			}
+			
+		}catch(Exception ex){
+			System.out.println("Error - aca.empleado.EmpPersonalLista|mapTipoEmpleados|:"+ex);
+		}finally{
+			if (rs!=null) rs.close();
+			if (st!=null) st.close();
+		}				
+		
+		return map;
 	}
 	
 }
